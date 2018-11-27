533e087a0cf12717e00f34207bc167066bd85747
nmessa <nmessa@sau16.org>
2018-11-27 15:17:23 -0500

    Add files via upload

---
+++ backwords-start.py
@@ -0,0 +1,13 @@
+##backwords.py
+##Author: nmessa
+##Date: 11.27.2018
+
+#user enters a word and stores in word
+#Add code here
+
+
+#if the word has more than 5 characters print it backwards
+#or else print it normally
+#Add code here
+
+
